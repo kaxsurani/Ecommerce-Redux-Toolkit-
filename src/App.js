a7abe9cb70b80e77628fab52358c625c0712770b
Textutils <167845453+kaxsurani@users.noreply.github.com>
2024-09-28 15:30:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import React from "react";
-import "./App.css";
-import Main from "./Components/Main/Main";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FilteredProducts from "./Components/FiltredProducts/FilteredProducts";
-import SingleProduct from "./Components/FiltredProducts/SingleProduct";
-import Login from "./Components/Login/Login";
+
 import { useSelector } from "react-redux";
 
 function App() {
@@ -16,20 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route
-            path="/"
-            element={authUser ? <Main></Main> : <Login></Login>}
           ></Route>
-
-          <Route
-            path="/filteredProducts/:type"
-            element={<FilteredProducts></FilteredProducts>}
-          ></Route>
-          <Route
-            path="/filteredProducts/:type/:id"
-            element={<SingleProduct></SingleProduct>}
-          ></Route>
-        </Routes>
-      </BrowserRouter>
+        <
     </div>
   );
 }
